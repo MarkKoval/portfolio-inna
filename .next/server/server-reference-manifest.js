265216f933369d71a866d0e719c8675b8938c0c0
Mark Koval <marekmark22@gmail.com>
2026-02-07 19:54:57 +0200

    asdfasdf

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"4BBwGcLCb+uqXKFsVDSOn/BP7MVXaknRlilivq7vkMc=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"\n}"
