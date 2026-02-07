cd41ca5b73e917872d356d3b11984308ed47998a
Mark Koval <marekmark22@gmail.com>
2026-02-07 20:02:00 +0200

    asdfasfdsadf

--- components/ui/Container.tsx
+++
@@ -1,5 +0,0 @@
-import { ReactNode } from 'react';
-
-export function Container({ children, className = '' }: { children: ReactNode; className?: string }) {
-  return <div className={`mx-auto w-full max-w-[1300px] px-5 md:px-8 ${className}`}>{children}</div>;
-}
